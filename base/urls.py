d8ca8396895ad9db6b826ba154fda168965c42a0
raigekhi <raigekhi@gmail.com>
2022-05-16 14:26:44 +0800

    put filter query on skills

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+
 from . import views
 
+
 urlpatterns = [
+    
+    
     path('',views.homePage, name = 'home'),
+    
+    
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
+from email.quoprimime import body_check
+from multiprocessing import context
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Skill
 
 # Create your views here.
 
 
 def homePage(request):
     projects = Project.objects.all()
-    return render(request, 'base/home.html',{'projects':projects}) 
+    detailedSkills = Skill.objects.exclude(body="")
+
+    skills = Skill.objects.filter(body="")
+
+    context = {"projects": projects, "skills": skills, "detailedSkills": detailedSkills}
+    return render(request, "base/home.html", context)

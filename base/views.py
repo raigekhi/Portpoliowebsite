8ec61f6592ac194df34f81857243da46af801ebd
raigekhi <raigekhi@gmail.com>
2022-05-16 12:37:13 +0800

    init

---
+++ base/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+
+
+def homePage(request):
+    projects = Project.objects.all()
+    return render(request, 'base/home.html',{'projects':projects}) 
